513a8b7e6d234c390727663ec2fc404f849c3b47
Steven <unclearwillo@gmail.com>
2022-07-29 20:11:57 -0400

    commit tree 2b07da3576196a1a1b25fdfc60701854d1aaa60e

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",() => {
+  const hearts = document.querySelectorAll(".like-glyph")
 
+  hearts.forEach(hearts => hearts.addEventListener("click", fillHeart))
 
-
+  function fillHeart(hearts){
+    mimicServerCall()
+    .then(() => {
+      if (hearts.target.innerText === EMPTY_HEART){
+        hearts.target.classList.add("activated-heart")
+        hearts.target.innerText = FULL_HEART;
+      }
+      else {
+      hearts.target.className = ""
+      hearts.target.innerText = EMPTY_HEART;
+      
+      }
+    
+    })
+    .catch((error) => {
+      console.log(error)
+      const getModal = document.getElementById("modal");
+      getModal.classList.toggle("hidden")
+      setTimeout(() => getModal.classList.toggle("hidden"), 3000)
+  })
+  }
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +47,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
